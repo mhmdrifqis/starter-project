51412d62500c823895460199d97f97406eddf6ae
mhmdrifqis <muhammadrifqis1999@gmail.com>
2026-01-15 11:23:46 +0700

    Day 3: Comprehensive Testing Guide

--- src/repositories/TaskRepository.js
+++ src/repositories/TaskRepository.js
@@ -1,3 +1,5 @@
+
+
 /**
  * Task Repository - Mengelola penyimpanan dan pengambilan data Task
  * 
@@ -7,6 +9,12 @@
  * - User-specific operations
  * - Statistics dan reporting
  */
+if (typeof require !== 'undefined' && typeof module !== 'undefined') {
+    if (typeof User === 'undefined') {
+        EnhancedTask = require('../models/EnhancedTask');
+    }
+}
+
 class TaskRepository {
     constructor(storageManager) {
         this.storage = storageManager;
--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -1,3 +1,4 @@
+const User = require('../models/User');
 /**
  * User Repository - Mengelola penyimpanan dan pengambilan data User
  * 
